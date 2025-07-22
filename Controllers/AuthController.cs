82ec62ace2ede2be61862ce382a5c9cbd1c19d27
chad solomon <chadcsoloman@gmail.com>
2025-07-21 22:00:52 -0400

    Added CreateToken method to AuthController that generates a JWT

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,10 @@ using DataBrokerAPI.Entities.DTOs;
 using DataBrokerAPI.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace DataBrokerAPI.Controllers
 {
@@ -11,9 +15,11 @@ namespace DataBrokerAPI.Controllers
     public class AuthController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public AuthController(IUnitOfWork unitOfWork)
+        private readonly IConfiguration _configuration;
+        public AuthController(IUnitOfWork unitOfWork, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
         [HttpPost("registar")]
         public ActionResult<CustomerDTO> Register(CustomerDTO request)
@@ -50,7 +56,39 @@ namespace DataBrokerAPI.Controllers
             {
                 return BadRequest("Username or password is incorrect");
             }
-            return Ok("Login successful");  
+
+            //Create a token for the customer
+            var token = CreateToken(customer);
+
+            return Ok(token);  
+        }
+
+        private string CreateToken(Customer customer)
+        {
+            //Create claims for the customer
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, customer.CustomerId.ToString()),
+                new Claim(ClaimTypes.Name, customer.Username)
+            };
+
+            //Symmetric Security Key - Signing key to ensure that the JWT actually came from this api
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("AppSettings:Token")!));
+
+            //Create the signing credentials using the key and the algorithm
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new JwtSecurityToken
+            (
+                issuer: _configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: _configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1), // Token expiration time
+                signingCredentials: creds
+            );
+
+            //Create the token using the JwtSecurityTokenHandler and return it
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
