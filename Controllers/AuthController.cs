e61cf88cb42699166e2027cd068a98d75347768d
chad solomon <chadcsoloman@gmail.com>
2025-07-23 20:10:32 -0400

    Starting to implement RefreshTokens.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DataBrokerAPI.Controllers
@@ -105,6 +106,39 @@ namespace DataBrokerAPI.Controllers
 
             //Create the token using the JwtSecurityTokenHandler and return it
             return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
+        private string GenerateRefreshToken()
+        {
+            //Create a byte array
+            var randomNumber = new byte[32];
+
+            //Create an instance of Random Number Generator class
+            using var rng = RandomNumberGenerator.Create();
+
+            //Fill the byte array with random bytes
+            rng.GetBytes(randomNumber);
+
+            //Convert the byte array to a Base64 string and return it
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private async Task<string> GenerateAndSaveRefreshToken(Customer customer)
+        {
+            //Generate a new refresh token
+            var refreshToken = GenerateRefreshToken();
+
+            //Save the refresh token to the customer object
+            customer.RefreshToken = refreshToken;
+
+            //Set the refresh token expiry time to 1 days from now
+            customer.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(1);
+
+            //Save the changes to the database
+            _unitOfWork.Save();
+
+            //Return the refresh token
+            return refreshToken;
         }
     }
 }
--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -35,5 +35,17 @@
             get;
             set;
         } = string.Empty;
+
+        public string RefreshToken
+        {
+            get;
+            set;
+        } = string.Empty;
+
+        public DateTime? RefreshTokenExpiryTime
+        {
+            get;
+            set;
+        }
     }
 }
