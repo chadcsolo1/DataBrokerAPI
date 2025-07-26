ec84c25710d0c67bbdaa7c628806543bae808c9f
chad solomon <chadcsoloman@gmail.com>
2025-07-25 20:56:26 -0400

    Adding CreateCustomer method to the Customer Repository.

--- Repositories/Implementations/CustomerRepo.cs
+++ Repositories/Implementations/CustomerRepo.cs
@@ -15,9 +15,11 @@ namespace DataBrokerAPI.Repositories.Implementations
     public class CustomerRepo : ICustomerRepo
     {
         private readonly ApplicationDbContext _db;
-        public CustomerRepo(ApplicationDbContext db)
+        private readonly IUnitOfWork _unitOfWork;
+        public CustomerRepo(ApplicationDbContext db, IUnitOfWork unitOfWork)
         {
             _db = db ?? throw new ArgumentNullException(nameof(db));
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         }
         public async Task<Customer> GetCustomerByUsername(CustomerDTO request)
         {
@@ -29,13 +31,30 @@ namespace DataBrokerAPI.Repositories.Implementations
 
             var customer = _db.Customers.FirstOrDefault(c => c.Username == request.Username);
 
-            if (new PasswordHasher<Customer>().VerifyHashedPassword(customer, customer.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            return customer ?? throw new Exception("No customer found in the database.");
+        }
+
+        public async Task<string> CreateCustomer(Customer customer)
+        {
+            // Validate the customer object
+            if (customer == null)
             {
-                return null;
+                throw new ArgumentNullException(nameof(customer), "Customer cannot be null");
             }
 
 
-            return customer;
+            // Check if the customer already exists
+            var existingCustomer = _db.Customers.FirstOrDefault(c => c.Username == customer.Username);
+
+            if (existingCustomer != null)
+            {
+                throw new InvalidOperationException("Customer with this username already exists.");
+            }
+
+            // Save the new customer to the database
+            _db.Customers.Add(customer);
+            _unitOfWork.Save();  
+            return "CreateCustomer method not implemented yet.";
         }
 
 
